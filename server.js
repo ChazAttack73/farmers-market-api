294e9f07dc3223c6995decd5bc18f5ad3c282c3c
KaiKeanaaina <kaikeanaaina@gmail.com>
2016-02-16 15:29:40 -1000

    added a router

--- server.js
+++ server.js
@@ -4,12 +4,12 @@ var bodyParser = require('body-parser');
 var session = require('express-session');
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
-// var CONFIG = require('.config/config.js');
+var CONFIG = require('.config/config.js');
 
 // var db = require('.models');
 
-
-
+app.use(session(CONFIG.SESSION));
+app.use('/vendor', require('./routes/vendor.js'));
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended:true}));
 
