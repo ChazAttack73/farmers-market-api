4539982bf8e00347a1e7eb271488194891c89076
Chaz <cmtlum73@gmail.com>
2016-02-16 16:25:41 -1000

    starts database hookup

--- server.js
+++ server.js
@@ -2,13 +2,17 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 
+var db = require( './models' );
+
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended:true}));
 
-
+var Company = db.Company;
+var Product = db.Product;
 
 
 
 app.listen(3000,function(){
   console.log('CONNECTED');
+  db.sequelize.sync();
 });
