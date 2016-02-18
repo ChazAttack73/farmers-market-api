5bc932f8a5ee163c6dbe74903d89bb8b1befda25
KaiKeanaaina <kaikeanaaina@gmail.com>
2016-02-18 10:54:23 -1000

    vendor routes has more queries, the vendors have their own products, so the queries to the products are also on the vendor.js route

--- routes/vendor.js
+++ routes/vendor.js
@@ -3,12 +3,20 @@ var app = express();
 var router = express.Router();
 var db = require('./../models');
 var Vendor = db.Vendor;
+var Product = db.Product;
 var bodyParser = require('body-parser');
 
 router.use(bodyParser.json());
 
 router.get( '/', function ( req, res ) {
   Vendor.findAll()
+    .then( function ( vendors ) {
+      res.json( vendors );
+    });
+  });
+
+router.post( '/', function ( req, res ) {
+  Vendor.create(req.body)
     .then( function ( vendors ) {
       res.json( vendors );
     });
@@ -25,21 +33,25 @@ router.get( '/:id', function( req, res){
   });
 });
 
-router.post( '/', function ( req, res ) {
-  Vendor.create(
-    {
-      name: req.body.name,
-      password: req.body.password,
-      phone: req.body.phone,
-      email : req.body.email,
-      website: req.body.website,
-      description: req.body.description,
-      company_pic: req.body.company_pic
-    })
-    .then( function ( vendors ) {
-      res.json( vendors );
+router.put('/:id', function( req, res){
+  req.body.updatedAt = "now()";
+  Vendor.update(
+    req.body, {
+    where : {
+      id : req.params.id
+    }
+  })
+  .then(function(vendorUpdateCount){
+    return Vendor.findOne({
+      where:{
+        id:req.params.id
+      }
     });
+  })
+  .then(function(vendor){
+    res.json( vendor );
   });
+});
 
 router.delete('/:id', function( req, res){
   Vendor.destroy({
@@ -53,7 +65,46 @@ router.delete('/:id', function( req, res){
       res.json( vendors );
     });
   });
-
 });
+
+router.get('/:id/products/', function( req , res){
+  Product.findAll({
+    where:{
+      VendorId: req.params.id
+    }
+  })
+  .then( function ( products){
+    res.json ( products );
+  });
+});
+
+router.post('/:id/products/', function( req, res){
+  req.body.VendorId = req.params.id;
+  Product.create(req.body)
+  .then( function ( product ){
+    res.json( product );
+  });
+});
+
+router.put('/:id/products/:product', function( req, res){
+  req.body.updatedAt = "now()";
+  Product.update(
+    req.body, {
+    where : {
+      VendorId : req.params.product
+    }
+  })
+  .then(function(productUpdateCount){
+    return Product.findOne({
+      where:{
+        id:req.params.product
+      }
+    });
+  })
+  .then(function(product){
+    res.json( product );
+  });
+});
+
 
 module.exports = router;
