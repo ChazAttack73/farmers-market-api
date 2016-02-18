8b9afd3e0cfdf8f99ca2f880c39a7b1826c8b23f
Chaz <cmtlum73@gmail.com>
2016-02-17 16:45:16 -1000

    adds GET and POST for products

---
+++ routes/product.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var app = express();
+var router = express.Router();
+var db = require('./../models');
+var Product = db.Product;
+var bodyParser = require('body-parser');
+
+router.use(bodyParser.json());
+
+router.get( '/', function ( req, res ) {
+Product.findAll()
+  .then( function ( products ) {
+    res.json( products );
+  });
+});
+
+router.post( '/', function ( req, res ) {
+  Product.create(
+    {
+      name: req.body.name,
+      price: req.body.price,
+      quantity: req.body.quantity,
+      description: req.body.description,
+      product_picture : req.body.product_picture,
+      // VendorId: req.vendor.id
+    })
+    .then( function ( products ) {
+      res.json( products );
+    });
+  });
+
+module.exports = router;
