f669ad7ed952276c93ff820a82f338a199d3967d
Micah Koki <micah.koki@gmail.com>
2016-02-19 12:41:55 -1000

    progress on templates

--- public/js/controllers/event-controller.js
+++ public/js/controllers/event-controller.js
@@ -3,12 +3,6 @@
 angular.module('myApp')
 .controller('EventController', ['$scope', 'EventService', 'ProductService', '$rootScope', 'VendorService', '$location', '$localStorage', function($scope, EventService, $rootScope, ProductService, VendorService, $location, $localStorage){
   $scope.Events = [];
-  // $scope.Events = [{
-  //   name : "Manoa Marketplace",
-  //   address: "2752 Woodlawn Drive",
-  //   days: "Tuesday, Thursday, Sunday",
-  //   time: "7:00 AM to 11:00"
-  // }];
   $scope.vendor = {
     createdBy : $rootScope.creator_user
   };
