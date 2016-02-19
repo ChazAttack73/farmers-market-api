2011f8e407665a2a1c5682a603ead726b71e0532
Micah Koki <micah.koki@gmail.com>
2016-02-18 19:44:06 -1000

    edited landing page

--- public/js/controllers/event-controller.js
+++ public/js/controllers/event-controller.js
@@ -1,20 +1,19 @@
 "use strict";
 
 angular.module('myApp')
-.controller('EventController', ['$scope', 'EventService', '$rootScope', 'VendorService', '$location', '$localStorage', function($scope, EventService, $rootScope, VendorService, $location, $localStorage){
+.controller('EventController', ['$scope', 'EventService', 'ProductService', '$rootScope', 'VendorService', '$location', '$localStorage', function($scope, EventService, $rootScope, ProductService, VendorService, $location, $localStorage){
   $scope.Events = [{
     name : "Manoa Marketplace",
     address: "2752 Woodlawn Drive",
     days: "Tuesday, Thursday, Sunday",
     time: "7:00 AM to 11:00"
   }];
-  $scope.Events = [];
   $scope.vendor = {
     createdBy : $rootScope.creator_user
   };
-  $scope.ProductService = ProductService;
-  ProductService.getProducts().success(function(data){
-    $scope.Products = data;
+  $scope.EventService = EventService;
+  EventService.getEvents().success(function(data){
+    $scope.Events = data;
   });
 
   $scope.Vendors = [];
