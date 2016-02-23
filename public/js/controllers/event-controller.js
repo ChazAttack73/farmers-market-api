2109bfcca0038ea86a50a46dd0c8475b68395e05
Chaz <cmtlum73@gmail.com>
2016-02-22 20:41:26 -1000

    brings back event data object all upper cased

--- public/js/controllers/event-controller.js
+++ public/js/controllers/event-controller.js
@@ -6,24 +6,23 @@ angular.module('myApp')
   $scope.vendor = {
     createdBy : $rootScope.creator_user
   };
+
   $scope.EventService = EventService;
 
   EventService.getEvents().success(function(data){
-    $scope.Events = data;
+    for( var i = 0; i < data.length; i++ ){
+      var event = $scope.Events[i] = {};
+      event.name = data[i].name.toUpperCase();
+      event.days = data[i].days.toUpperCase();
+      event.time = data[i].time.toUpperCase();
+      event.address = data[i].address.toUpperCase();
+    }
   });
 
   $scope.registerEvent = function(theEvent){
-
     EventService.addEvent(theEvent)
     .success(function(data){
       $location.url('/');
     });
-
-
   };
-
-
-
-
 }]);
-
