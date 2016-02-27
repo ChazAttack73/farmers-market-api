c574408933550bc108ea449a7dd1f72dd8fc2bd0
KaiKeanaaina <kaikeanaaina@gmail.com>
2016-02-26 18:59:25 -1000

    changed middle were from payment to stripe

--- routes/stripe.js
+++ routes/stripe.js
@@ -11,7 +11,7 @@ var stripe = require("stripe")("sk_test_L3fF5CjV33nFCv2dg7vcKQmz");
 router.use(bodyParser.json({ extended: false }));
 
 router.post('/:id',function( req, res){
-  console.log(5555555555);
+
   Order.create({
     productQuantity : req.body.productQuantity,
     totalCost : req.body.amount,
@@ -19,13 +19,11 @@ router.post('/:id',function( req, res){
     ProductId : req.body.product
   }).
   then(function(order){
-    console.log(666666666666);
-    Payment.create({
+       Payment.create({
       token : req.body.token,
       OrderId : order.id
     })
     .then(function(data){
-      console.log(77777777777777);
       var charge = stripe.charges.create({
         amount: req.body.amount, // amount in cents, again
         currency: "usd",
@@ -38,9 +36,6 @@ router.post('/:id',function( req, res){
         }
 
       }, function(err, charge) {
-        console.log(88888888888,charge);
-        console.log(typeof req.body.productQuantity);
-
         if (err && err.type === 'StripeCardError') {
           // The card has been declined
           return res.json('error');
@@ -55,9 +50,11 @@ router.post('/:id',function( req, res){
     });
 
   });
-
 });
 
+router.get('/callback', function(req, res){
+  res.redirect('/vendor/private');
+});
 
 module.exports = router;
 
